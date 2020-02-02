feddcb68ea3ac6ef9d48dbc5f5aba59e10898b1c
taltehan <taltehan@gmail.com>
2020-02-02 20:59:21 +0200

    updated ToM function

--- SI_paradigm/Code/functions/main_SI_ToM.m
+++ SI_paradigm/Code/functions/main_SI_ToM.m
@@ -19,7 +19,7 @@ Screen('Preference', 'SkipSyncTests', 1);
 file_name = sprintf('log_subject_%i_run_%i.txt', subject, run);
 output_dir = sprintf('Output_%s', experiment);
 fid = fopen(fullfile('..', output_dir, file_name), 'w');
-fprintf(fid,'Trial\tCond\tAudioStart\tAudioEnd\tImageEnd\tFixEnd\tAudioFN\tImageFN\tSjtChoice\tRT\n');
+fprintf(fid,'Trial\tCond\tStoryStart\tQuestionStart\tFixStart\tStoryFN\tQuestionFN\tSjtChoice\tRT\n');
 % Initialize
 warning off
 HideCursor
@@ -79,97 +79,74 @@ for i = 1:stimuli.num_trials
     log_row{1} = i;
     log_row{2} = stimuli.condition_name{i};
     log_row{3} = toc(grandStart);
-        choice_str = '-'; subject_responses{i, 1, 1} = '-'; subject_responses{i, 2, 1} = -999; cnt_presses = 0;
-        %% Present visual stimulus
-        % if ~isempty(stimuli.visual(i).image)
-        Screen('DrawTexture', win, stimuli.visual(i).image, [], params.visualCoords);
-        Screen('Flip',win);
+    choice_str = '-'; subject_responses{i, 1, 1} = '-'; subject_responses{i, 2, 1} = -999; cnt_presses = 0;
+    %% Present Story
+    Screen('DrawTexture', win, stimuli.visual(i).image, [], params.visualCoords);
+    Screen('Flip',win);
+    % wait for the story presentation time
+    while toc(trial) < params.story_duration
+    end
+    
+    %% Present Question
+    Screen('DrawTexture', win, stimuli.visual(i).question, [], params.visualCoords);
+    Screen('Flip',win);
+    log_row{4} = toc(grandStart);
+    
+    %% Wait for response
+    rt = tic;
+    while toc(trial) < params.visual_duration
+        press_event = false;
+        [~, ~, keyCode] = KbCheck;
+        if any(keyCode([middleKey2,KbName('3#')]))
+            press_event = true; choice_str = '3';
+        elseif any(keyCode([leftKey,KbName('1!')]))
+            press_event = true; choice_str = '1';                    
+        elseif any(keyCode([middleKey1,KbName('2@')]))
+            press_event = true; choice_str = '2';
+        elseif any(keyCode([rightKey,KbName('4$')]))
+            press_event = true; choice_str = '4';
+        elseif keyCode(escKey)
+            DisableKeysForKbCheck([]);
+            Screen('CloseAll');
+            ShowCursor
+            return
+        end           
 
-        %% Present auditory stimulus
-        if params.enable_audio
-            PsychPortAudio('Start', pahandle,1);
-            PsychPortAudio('DeleteBuffer');
-            sounddata = stimuli.ping.y(:,1);
-            PsychPortAudio('FillBuffer', pahandle, sounddata');
-            while toc(trial) < params.audio_duration-0.2 %wait for the beep
-            end
-            PsychPortAudio('Start', pahandle,1);
-            while toc(trial) < params.audio_duration % Wait
-            end
-
-            PsychPortAudio('DeleteBuffer');
-            if i<stimuli.num_trials
-                sounddata = stimuli.auditory(i+1).y(:,1);
-                PsychPortAudio('FillBuffer', pahandle, sounddata');
-            end
-        end
-
-        log_row{4} = toc(grandStart);
-%             fprintf(fid,'Trial %i audio ended from grand start: %f\n', i, toc(grandStart));
-%             fprintf(fid,'Trial %i audio ended from trial start: %f\n', i, toc(trial));
-
-        %% Wait for response
-        rt = tic;
-        while toc(trial) < params.visual_duration
-            press_event = false;
-            [~, ~, keyCode] = KbCheck;
-            if any(keyCode([middleKey2,KbName('3#')]))
-                press_event = true; choice_str = '3';
-            elseif any(keyCode([leftKey,KbName('1!')]))
-                press_event = true; choice_str = '1';                    
-            elseif any(keyCode([middleKey1,KbName('2@')]))
-                press_event = true; choice_str = '2';
-            elseif any(keyCode([rightKey,KbName('4$')]))
-                press_event = true; choice_str = '4';
-            elseif keyCode(escKey)
-                DisableKeysForKbCheck([]);
-                Screen('CloseAll');
-                ShowCursor
-                return
-            end           
-
-            if press_event
-                if cnt_presses > 0
-                    if ~strcmp(subject_responses{i,1,cnt_presses}, choice_str)
-                        cnt_presses = cnt_presses + 1;
-                        subject_responses{i, 1, cnt_presses} = choice_str;
-                        subject_responses{i, 2, cnt_presses} = toc(rt);
-                    end
-                else
+        if press_event
+            if cnt_presses > 0
+                if ~strcmp(subject_responses{i,1,cnt_presses}, choice_str)
                     cnt_presses = cnt_presses + 1;
                     subject_responses{i, 1, cnt_presses} = choice_str;
                     subject_responses{i, 2, cnt_presses} = toc(rt);
                 end
+            else
+                cnt_presses = cnt_presses + 1;
+                subject_responses{i, 1, cnt_presses} = choice_str;
+                subject_responses{i, 2, cnt_presses} = toc(rt);
             end
-
         end
-        % end
-        log_row{5} = toc(grandStart);
 
-        %% Fixation 
+    end
+
+        %% Fixation
+        log_row{5} = toc(grandStart);
+        % Show subject's choice on the top-right corner
         Screen(win, 'TextSize', 30);
         DrawFormattedText(win, choice_str, 0.95*rect(3), 0.05*rect(4), params.fixation_color)
+        % Show fixation
         Screen(win, 'TextSize', params.fixation_size);
         % DrawFormattedText2(fixation_cache);
         DrawFormattedText(win, '+', 'center', 'center', params.fixation_color);
-        % Screen('DrawText',win,'+', rect(3)/2, rect(4)/2, params.fixation_color);
         Screen('Flip',win);
 
         while toc(grandStart) < stimuli.end_trial(i) % Wait
         end
-        log_row{6} = toc(grandStart);
-%             fprintf(fid,'Trial %i fixation ended from grand start: %f\n', i, toc(grandStart));
-%             fprintf(fid,'Trial %i fixation ended from trial start: %f\n', i, toc(trial));
-        if params.enable_audio
-            log_row{7} = stimuli.auditory(i).auditori_file_name;
-        else
-            log_row{7} = '0';
-        end
-        log_row{8} = stimuli.visual(i).image_file_name;
-        log_row{9} = subject_responses{i, 1, 1};
-        log_row{10} = subject_responses{i, 2, 1};
+        log_row{6} = stimuli.visual(i).image_file_name;
+        log_row{7} = stimuli.visual(i).question_file_name;
+        log_row{8} = subject_responses{i, 1, 1};
+        log_row{9} = subject_responses{i, 2, 1};
 
-    curr_row = sprintf('%i\t%s\t%0.6f\t%0.6f\t%0.6f\t%0.6f\t%s\t%s\t%s\t%0.6f', log_row{1}, log_row{2}, log_row{3}, log_row{4}, log_row{5}, log_row{6}, log_row{7}, log_row{8}, log_row{9}, log_row{10});
+    curr_row = sprintf('%i\t%s\t%0.6f\t%0.6f\t%0.6f\t%s\t%s\t%s\t%0.6f', log_row{1}, log_row{2}, log_row{3}, log_row{4}, log_row{5}, log_row{6}, log_row{7}, log_row{8}, log_row{9});
     fprintf(fid,'%s\n', curr_row);
     
 end
